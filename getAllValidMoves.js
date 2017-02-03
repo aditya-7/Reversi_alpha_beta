f8bf3a8c33e9c6809fef3b8593428644058d3014
aditya <aditya@lnt.com>
2017-02-03 19:13:44 +0530

    get all possible moves - done

--- getAllValidMoves.js
+++ getAllValidMoves.js
@@ -1,11 +1,11 @@
 var _ = require('lodash');
 
-module.exports = function getAllValidMoves(board, whites, blacks, player){
+module.exports = function getAllValidMoves(board, players, opponents, color){
   var X, O;
-  if(player == 'W') {
+  if(color == 'W') {
     X = 'X';
     O = 'O';
-  }else { // if player == 'B'
+  }else { // if color == 'B'
     X = 'O';
     O = 'X';
   }
@@ -24,6 +24,13 @@ module.exports = function getAllValidMoves(board, whites, blacks, player){
     var new_row = (coin.row)+row;
     var new_col = (coin.col)+col;
     if(
+      new_row > 7 || new_col > 7 || new_row < 0 || new_col < 0 ||// check if we reached the end of the board
+      current_board[new_row][new_col] === X // check if the current movement gives you a white coin
+    ){
+      // Either of the 2 conditions makes it is an illegal move.
+      return false;
+    }
+    if(
       // check if the first movement gives you a blank square
       current_board[new_row][new_col] == '*' &&
       (current_coin.row + row) == new_row &&
@@ -31,34 +38,27 @@ module.exports = function getAllValidMoves(board, whites, blacks, player){
     ){
       return false;
     }
-    if(
-      new_row > 7 || new_col > 7 || new_row < 0 || new_col < 0 ||// check if we reached the end of the board
-      current_board[new_row][new_col] === X // check if the current movement gives you a white coin
-    ){
-      // Either of the 2 conditions makes it is an illegal move.
-      return false;
-    }
     else {  //in here it can only be a * or O, as we are cutting out the chance to show 'X' in the above if.
-      if(current_board[new_row][new_col] === O){ //check if the current movement gives you a black coin
+      if(current_board[new_row][new_col] === O){ //check if the current movement gives you a opponent coin
         coin = {row: new_row, col: new_col};  //update coin to the next square through the same movement
         current_board[new_row][new_col] = X;
         traverseRecursively(current_board, coin, {row:row, col:col}); //traverse recursively through the same path
       }else {
         if((new_row + row < 8) && (new_col + col < 8) && (new_row + row >= 0) && (new_col + col >= 0)) {
-          //if the black coin is not at the end of the board after all this, then we can be sure that it is a valid move.
+          //if the opponent coin is not at the end of the board after all this, then we can be sure that it is a valid move.
           current_board[new_row][new_col] = X;
           adjacent_coin = {row: new_row, col: new_col};
           allValidMoves.push({board:current_board, move: row_col, coin: adjacent_coin});
         }else{
-          //if the black coin is at the end of the board, its still not a valid move.
+          //if the opponent coin is at the end of the board, its still not a valid move.
           return false;
         }
       }
     }
   }
 
-  for(var j=0; j<whites.length; j++){
-    current_coin = whites[j];
+  for(var j=0; j<players.length; j++){
+    current_coin = players[j];
     for(var i=0; i<possible_movements.length; i++){
       //cloning the object because we need to maintain the original state of the board for the next coin
       traverseRecursively(JSON.parse(JSON.stringify(board)), current_coin, possible_movements[i]);
@@ -67,7 +67,7 @@ module.exports = function getAllValidMoves(board, whites, blacks, player){
   var final_board_values = {};  //Where the final board values for current move will be present.
 
   function addMatrix(valid_move){
-    // this method will replace other blacks to white wherever it is needed
+    // this method will replace other opponent to player wherever it is needed
     var board_now = final_board_values[valid_move.coin.row + ',' + valid_move.coin.col];
     for(var i=0; i<8; i++){
       for(var j=0; j<8; j++){
@@ -81,10 +81,10 @@ module.exports = function getAllValidMoves(board, whites, blacks, player){
 
   for (var i = 1; i <= allValidMoves.length; i++) {
     if(final_board_values[allValidMoves[i-1].coin.row + ',' + allValidMoves[i-1].coin.col] !== undefined){
-      // if the move has already been played with another white coin
+      // if the move has already been played with another player coin
       addMatrix(allValidMoves[i-1]);
     }else{
-      // if the move has not been played with another white coin
+      // if the move has not been played with another player coin
       final_board_values[allValidMoves[i-1].coin.row + ',' + allValidMoves[i-1].coin.col] = allValidMoves[i-1].board;
     }
   }
--- reversi.js
+++ reversi.js
@@ -1,8 +1,11 @@
 
 var _ = require('lodash');
 var getAllValidMoves = require('./getAllValidMoves');
-var whites = [{row: 3, col:5},{row: 4, col:4}, {row:5, col:0}, {row:1, col:0}];
-var blacks = [{row: 3, col:3 },{row: 3, col:4},{row: 2, col:4}, {row:4, col:1}, {row:2, col:1}];
+// var whites = [{row: 3, col:5},{row: 4, col:4}, {row:5, col:0}, {row:1, col:0}];
+// var blacks = [{row: 3, col:3 },{row: 3, col:4},{row: 2, col:4}, {row:4, col:1}, {row:2, col:1}];
+
+var whites = [{row: 0, col:0}];
+var blacks = [{row:1,col:1}, {row:2, col:2}];
 var board = [];
 
 //Note that board starts from [0][0] and ends at [7][7] here.
@@ -34,40 +37,87 @@ function printBoard (some_board){
 }
 
 function updateWhiteBlack (board){
-  whites = [];
-  blacks = [];
+  var new_whites = [];
+  var new_blacks = [];
   for(var i=0; i<8; i++){
     for(var j=0; j<8; j++){
       if(board[i][j] === 'X'){
-        whites.push({row:i, col:j});
+        new_whites.push({row:i, col:j});
       }else if(board[i][j] === 'O'){
-        blacks.push({row:i, col:j});
+        new_blacks.push({row:i, col:j});
       }
     }
   }
-
+  return {
+    whites: new_whites,
+    blacks: new_blacks
+  }
 }
 
 console.log('\n\n********ORIGINAL STATE OF BOARD********');
 printBoard(board); // print the actual board
 
-var valid_moves_now = getAllValidMoves(board, whites, blacks, 'W');
-var count = 0;
-console.log('\n\n********STATE OF BOARD WITH ALL VALID MOVES********');
-_.forEach(valid_moves_now, function(board, key){
-  console.log('\n\nwhen new coin placed at : ' +  key + '\n');
-  printBoard(board);
-  updateWhiteBlack(board);
-  console.log('whites',whites);
-  console.log('blacks',blacks);
-  if(count++ === 3){
-    console.log('\n\n*****************move of black');
-    console.log('\n\n********ORIGINAL STATE OF BOARD********');
-    printBoard(board)
-    var valid_moves_now = getAllValidMoves(board, blacks, whites, 'B');
-    _.forEach(valid_moves_now, function(board, key){
-      console.log('\n\nwhen new coin placed at : ' +  key + '\n');
-      printBoard(board);
+var childNodes;
+var DEPTH = 3;
+var allChildsAtDepth;
+var original_player = 'W';
+
+function checIfTerminalNode(board, level){
+  if(level == DEPTH) return findWeightOfTerminalNode(board);
+  var checkFor = 'X';
+  var value = 0;
+  if(original_player == 'B') checkFor = '0';
+  for(var i=0; i<8; i++){
+    for(var j=0; j<8; j++){
+      if(board[i][j] === '*') continue;
+      if(board[i][j] === checkFor) value+= weights[i][j];
+      else value-= weights[i][j];
+    }
+  }
+}
+
+var root = {
+  key : 'root',
+  value: null,    // Value which is determined using the heuristic function/ alpha-beta algorithm.
+  parent: null,   // The parent of this node; for root node it will be null.
+  children : [],  // If the length of array is null, it means its a terminal node.
+  state: board,   // The state of the board at this node.
+  player: null, // Based on who's playing first
+  level: 0
+};
+
+childNodes = [root];
+allChildsAtDepth = [childNodes];
+
+for(var i=1; i<= DEPTH; i++){
+  allChildsAtDepth[i] = [];
+  for(var j=0; j<childNodes.length; j++){
+    var coins = updateWhiteBlack(childNodes[j].state);
+    var pl, op;
+    if(i%2 == 1){ //player is white
+      pl = coins.whites;
+      op = coins.blacks;
+      player = 'W';
+    }else { //player is black
+      pl = coins.blacks;
+      op = coins.whites;
+      player = 'B';
+    }
+    var children = getAllValidMoves(childNodes[j].state, pl, op, player);
+    childNodes[j].children = children;
+    _.forEach(children, function(board, key){
+      // checIfTerminalNode(board, i);
+      allChildsAtDepth[i].push({
+        key: childNodes[j].key + '->' + key,
+        value : null,//checIfTerminalNode(children[i]),
+        parent: childNodes[j].key,
+        children : [],
+        state: board,
+        player: player,
+        level: i+1
+      });
     });
   }
-});
+  childNodes = allChildsAtDepth[i];
+}
+console.log('allChildsAtDepth \n', allChildsAtDepth);
