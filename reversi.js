bfef9b8b2679ad097e7544778c03e14c44add8a7
Aditya Kishore <aditya.kishore@lnttechservices.com>
2017-02-04 18:26:40 +0530

    all paths traversed succesfully

--- reversi.js
+++ reversi.js
@@ -69,7 +69,7 @@ var weights = [
   [6, -3, 4, 0, 0, 4, -3, 6],
   [8, -4, 7, 4, 4, 7, -4, 8],
   [-8, -24, -4, -3, -3, -4, -24, -8],
-  [99, -8, 8, 6, 6, 8, -8, 99],
+  [99, -8, 8, 6, 6, 8, -8, 99]
 ];
 
 function findWeightOfBoard(board, level){
@@ -96,11 +96,35 @@ function findWeightOfBoard(board, level){
   return value;
 }
 
+function convertToBoardMove(move){
+  var col;
+  switch (move[2]) {
+    case '0':
+      col = 'a'; break;
+    case '1':
+      col = 'b'; break;
+    case '2':
+      col = 'c'; break;
+    case '3':
+      col = 'd'; break;
+    case '4':
+      col = 'e'; break;
+    case '5':
+      col = 'f'; break;
+    case '6':
+      col = 'f'; break;
+    default:  //case '7'
+      col = 'h';
+  }
+  var row = Number(move[0]) + 1;
+  return col+row;
+}
+
 var root = {
   key : 'root',
   value: null,    // Value which is determined using the heuristic function/ alpha-beta algorithm.
   parent: null,   // The parent of this node; for root node it will be null.
-  children : {},  // If the length of array is null, it means its a terminal node.
+  children : [],  // If the length of array is null, it means its a terminal node.
   state: board,   // The state of the board at this node.
   player: null,   // Based on who's playing first
   level: 0,
@@ -111,7 +135,7 @@ childNodes = [root];
 var allChildsAtDepth = {
   'root' : root
 };
-var allMoves = ['root'];
+var allKeys = ['root'];
 var temp_nodes_array = [];
 for(var i=1; i<= DEPTH; i++){
   temp_nodes_array = [];
@@ -128,32 +152,52 @@ for(var i=1; i<= DEPTH; i++){
       player = 'B';
     }
     var children = getAllValidMoves(childNodes[j].state, pl, op, player);
-    childNodes[j].children = children;  // Is this useful ? I dont think so!
     _.forEach(children, function(board, key){
       var some_key = childNodes[j].key + '->' + key;
       var some_value = findWeightOfBoard(board, i);
       temp_nodes_array.push({
         key: some_key,  // is this useful? I dont think so!
-        move: key,  // to show the move made
+        move: convertToBoardMove(key),  // to show the move made
         value : some_value,
         parent: childNodes[j].key,
-        children : {},
+        children : [],
         state: board,
         player: player,
         level: i,
       });
-      allMoves.push(some_key);
+      allChildsAtDepth[childNodes[j].key].children.push(some_key);
+      childNodes[j].children.push(some_key);
+      allKeys.push(some_key);
       allChildsAtDepth[some_key] = JSON.parse(JSON.stringify(temp_nodes_array[temp_nodes_array.length-1]));
     });
   }
   childNodes = temp_nodes_array;
 }
 
-allMoves.sort();
-// console.log('allMoves', allMoves);
-// console.log('allChildsAtDepth', allChildsAtDepth);
-console.log('UniqueKey\t\t\t\t\tMove');
-// for(var i=allMoves.length-1; i>=0; i--){
-for(var i=0; i<allMoves.length-1; i++){
-  console.log(allMoves[i] + '\t\t\t\t' + allChildsAtDepth[allMoves[i]].value);
+allKeys.sort();
+console.log(allKeys);
+var allMovesInTraversal = ['root'];
+for(var i=1; i<allKeys.length; i++){
+  if(allKeys[i].indexOf(allKeys[i-1]) > -1){
+    allMovesInTraversal.push(allKeys[i]);
+  }
+  else{
+    var some_key = allKeys[i-1  ];
+    var some_array = [];
+    while(allKeys[i].indexOf(some_key) === -1){
+      some_key = some_key.substring(0,some_key.length-5);
+      // some_array.unshift(some_key);
+      allMovesInTraversal.push(some_key);
+    }
+    for(var j=some_array.length-1; j>0; j--){
+      if(some_array[j] !== some_array[j-1]){
+        allMovesInTraversal.push(some_array[j]);
+      }
+    }
+    allMovesInTraversal.push(allKeys[i]);
+  }
+}
+console.log('final traversal: ',allMovesInTraversal);
+for(var i=0;i<allMovesInTraversal.length;i++){
+  console.log(allChildsAtDepth[allMovesInTraversal[i]].move + ' ' + allChildsAtDepth[allMovesInTraversal[i]].value)
 }
